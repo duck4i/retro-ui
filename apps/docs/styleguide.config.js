aaf79b9db0e1f57e4f5221cdc977c56ce82c617d
duck4i <mihailogazda@gmail.com>
2025-01-14 21:32:41 +0000

    disgusting docs

--- apps/docs/styleguide.config.js
+++ apps/docs/styleguide.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    title: "@duck4i/retro-ui",
+    title: "retro-ui",
     styleguideDir: './dist',
     components: '../../packages/ui/components/**/*.{ts,tsx}',
     /*
@@ -34,19 +34,57 @@ module.exports = {
                     use: [
                         'style-loader',
                         {
-                          loader: 'css-loader',
-                          options: {
-                            modules: {
-                              namedExport: false,
+                            loader: 'css-loader',
+                            options: {
+                                modules: {
+                                    namedExport: false,
+                                },
                             },
-                          },
                         },
-                      ],
+                    ],
                 }
             ],
         },
         resolve: {
             extensions: ['.js', '.jsx', '.ts', '.tsx'],
         },
-    }
+    },
+    theme: {
+        color: {
+            base: "yellow",
+            light: 'white',
+            lightest: 'white',
+            sidebarBackground: "black",
+            baseBackground: "black",
+            codeBackground: 'black',
+            border: 'blue',
+            name: 'white',
+            type: 'magenta',
+            link: '#008400',
+            linkHover: 'cyan',
+        },
+        fontFamily: {
+            base: 'monospace'
+        },
+        borderRadius: 0
+    },
+    styles: function (theme) {
+        return {
+            Logo: {
+                logo: {
+                    color: "yellow"
+                }
+            },
+            SectionHeading: {
+                sectionName: {
+                    color: 'magenta',
+                }
+            }
+        }
+    },
+    getComponentPathLine(componentPath) {
+        const name = path.basename(componentPath, '.jsx');
+        // You can customize this string however you want
+        return `import { ${name.split('.')[0]} } from '@duck4i/retro-ui'`;
+    },
 };
